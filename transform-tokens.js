1d1b659fe001fd5212d43af56262f5a2e4a3dd70
artleitch <hello@artleitch.com>
2021-11-29 14:58:28 -0500

    add transformer for fonts

--- transform-tokens.js
+++ transform-tokens.js
@@ -1,53 +1,81 @@
-const StyleDictionary = require('style-dictionary')
-const baseConfig = require('./config.json')
+const StyleDictionary = require("style-dictionary");
+const baseConfig = require("./config.json");
 
+const notDefault = (value, defaultValue) =>
+  value !== defaultValue ? value : "";
 
-StyleDictionary.registerTransform({
-  name: 'size/px',
-  type: 'value',
-  matcher: token => {
-    return token.unit === 'pixel' && token.value !== 0
-  },
-  transformer: token => {
-    return `${token.value}px`
-  },
-})
+const fontFamily = ({ fontFamily }, { fontFamilies } = {}) =>
+  fontFamilies && fontFamilies[fontFamily]
+    ? fontFamilies[fontFamily]
+    : fontFamily;
 
 StyleDictionary.registerTransform({
-  name: 'size/percent',
-  type: 'value',
-  matcher: token => {
-    return token.unit === 'percent' && token.value !== 0
+  name: "web/font",
+  type: "value",
+    matcher: function (token) {
+      console.log('MATCHER', token.type)
+    return token.type === "custom-fontStyle";
   },
-  transformer: token => {
-    return `${token.value}%`
+    transformer: function ({ value: font }, { options }) {
+      console.log('YOU HIT ME')
+    return `${notDefault(font.fontStretch, "normal")} ${notDefault(
+      font.fontStyle,
+      "normal"
+    )} ${font.fontWeight} ${font.fontSize}/${font.lineHeight} ${fontFamily(
+      font,
+      options
+    )}`.trim();
   },
-})
+});
+
+StyleDictionary.registerTransform({
+  name: "size/px",
+  type: "value",
+  matcher: (token) => {
+      
+    return token.unit === "pixel" && token.value !== 0;
+  },
+  transformer: (token) => {
+    return `${token.value}px`;
+  },
+});
+
+StyleDictionary.registerTransform({
+  name: "size/percent",
+  type: "value",
+  matcher: (token) => {
+    return token.unit === "percent" && token.value !== 0;
+  },
+  transformer: (token) => {
+    return `${token.value}%`;
+  },
+});
 
 StyleDictionary.registerTransformGroup({
-  name: 'custom/css',
-  transforms: StyleDictionary.transformGroup['css'].concat([
-    'size/px',
-    'size/percent',
+  name: "custom/css",
+  transforms: StyleDictionary.transformGroup["css"].concat([
+    "size/px",
+    "size/percent",
   ]),
-})
+});
 
 StyleDictionary.registerTransformGroup({
-  name: 'custom/less',
-  transforms: StyleDictionary.transformGroup['less'].concat([
-    'size/px',
-    'size/percent',
+  name: "custom/less",
+  transforms: StyleDictionary.transformGroup["less"].concat([
+    "size/px",
+    "size/percent",
   ]),
-})
+});
 
 StyleDictionary.registerTransformGroup({
-  name: 'custom/scss',
-  transforms: StyleDictionary.transformGroup['less'].concat([
-    'size/px',
-    'size/percent',
+  name: "custom/scss",
+  transforms: StyleDictionary.transformGroup["less"].concat([
+    "size/px",
+      "size/percent",
+    'web/font',
   ]),
-})
+});
 
-const StyleDictionaryExtended = StyleDictionary.extend(baseConfig)
+const StyleDictionaryExtended = StyleDictionary.extend(baseConfig);
 
-StyleDictionaryExtended.buildAllPlatforms()
+StyleDictionaryExtended.buildAllPlatforms();
